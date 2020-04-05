365bdad6c326b9ae82141e1044d135acd00d3651
Gcc <gcc@Gccs-MBP.verizon.net>
2020-04-05 18:10:03 -0400

    login

--- src/containers/Prototype.js
+++ src/containers/Prototype.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import PrototypeContainer from "../components/PrototypeContainer";
+import LoginComponent from "../components/LoginComponent";
 
 class Prototype extends React.Component {
 
@@ -13,13 +14,21 @@ class Prototype extends React.Component {
                         path={"/"}
                         exact={true}
                         render={(props) =>
+                            <LoginComponent
+                                {...props}/>}
+                    />
+
+                    <Route
+                        path={"/search"}
+                        exact={true}
+                        render={(props) =>
                             <PrototypeContainer
                                 {...props}/>}
                     />
 
 
                     <Route
-                        path={"/:university"}
+                        path={"/search/:university"}
                         exact={true}
                         render={(props) =>
                             <PrototypeContainer
